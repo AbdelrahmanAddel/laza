646614edbc2de7626841fe67c826894236fe3fbe
Abdelrahman <Aa5282243@gmail.com>
2025-10-11 14:06:13 +0300

    .

--- lib/features/signup/domain/repositories/signup_repository.dart
+++ lib/features/signup/domain/repositories/signup_repository.dart
@@ -1,7 +1,6 @@
-import 'package:dartz/dartz.dart';
 import 'package:laza/features/signup/data/models/signup_request_model.dart';
 import 'package:laza/features/signup/data/models/signup_response_model.dart';
 
 abstract class SignupRepository {
-Future<Either<String, SignupResponseModel>> signup(SignupRequestModel request);
+  Future<SignupResponseModel> signup(SignupRequestModel request);
 }
